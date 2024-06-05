6efc50d3ed4a76328987abcdeb7b1282a406efbc
Satur42 <sambraunproductions@gmail.com>
2024-06-05 15:53:49 +0200

    added "get_device_samplerate" to audio_player

--- Example/app/audio_player.py
+++ Example/app/audio_player.py
@@ -1,10 +1,11 @@
 import numpy as np
 import sounddevice as sd
+import argparse
 
 class AudioPlayer:
 
     def __init__(self):
-        self.fs = 44100
+        self.fs = self.get_device_samplerate()
         self.beep_duration = 10
         self.volume = 0.5
         self.frequency = 440
@@ -24,3 +25,43 @@ class AudioPlayer:
         self.volume = volume
         self.beep_duration = duration
         sd.play(self.generate_tone(), self.fs)
+
+
+    def int_or_str(self, text):
+        """Helper function for argument parsing."""
+        try:
+            return int(text)
+        except ValueError:
+            return text
+
+
+    def get_device_samplerate(self):
+        """gets current samplerate from the selected audio output device
+
+        Returns:
+            float: samplerate of current sound device
+        """
+        parser = argparse.ArgumentParser(add_help=False)
+        parser.add_argument(
+            '-l', '--list-devices', action='store_true',
+            help='show list of audio devices and exit')
+        args, remaining = parser.parse_known_args()
+        if args.list_devices:
+            print(sd.query_devices())
+            parser.exit(0)
+        parser = argparse.ArgumentParser(
+            description=__doc__,
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            parents=[parser])
+        parser.add_argument(
+            'frequency', nargs='?', metavar='FREQUENCY', type=float, default=500,
+            help='frequency in Hz (default: %(default)s)')
+        parser.add_argument(
+            '-d', '--device', type=self.int_or_str,
+            help='output device (numeric ID or substring)')
+        parser.add_argument(
+            '-a', '--amplitude', type=float, default=0.2,
+            help='amplitude (default: %(default)s)')
+        args = parser.parse_args(remaining)
+        return sd.query_devices(args.device, 'output')['default_samplerate']
+    
